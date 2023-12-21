33f7d51e614a9e8c90c9c7596b14fb16fa0b6b7a
nicolas200518 <145553778+nicolas200518@users.noreply.github.com>
2023-12-21 20:22:49 +0100

    Update remotoNALDC.java

--- remotoNALDC/src/remotoNALDC/remotoNALDC.java
+++ remotoNALDC/src/remotoNALDC/remotoNALDC.java
@@ -4,7 +4,7 @@ public class remotoNALDC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Estoy en github y modifico el documento de java");
 	}
 
 }
